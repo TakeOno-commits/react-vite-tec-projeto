f7fe67dc4317db3514d46f5232a7cd2b2d8d14fc
wagnermarques <wagnerdocri@gmail.com>
2026-09-09 22:28:56 -0300

    09-09

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+funcion App(){
+    return (
+        <div className="bg-light min-vh-100 py-4">
+            <div clasName="container">
+                <header className="row mb-4">
+                    <div className="col text-center">
+                        <h1 className="display-5 fw-bold text-primary">
+                            Projeto React + Bootsrap Grid
+                        </h1>
+                        <p className='lead text-secondary'>
+                            Exemplo de layout responsivo utilizando o sistema de colunas do Boostrap                            
+                        </p>
+                        
+
+                    </div>
+                </header>
+            </div>
+        </div>
+    )
+
+}
+
+export default App;
